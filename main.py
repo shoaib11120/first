1e043f4905b909fcfebb7af68aad24626f16c51c
shoaib11120 <mdshoaibkhan.1999@gmail.com>
2020-06-07 16:51:23 +0530

    Day 2! Added sign up form

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import web
 urls = (
 	'/','Welcome',
 	'/home','Home',
-	'/profile','Profile'
+	'/profile','Profile',
+	'/profile/sign_up','Sign_up'
 )
 rende=web.template.render("views/resources/", base="base_layout")
 class Welcome:
@@ -17,6 +18,10 @@ class Home:
 	"""docstring for home"""
 	def GET(self):
 		return rende.home()
+class Sign_up:
+	"""docstring for home"""
+	def GET(self):
+		return rende.sign_up()
 if __name__ == "__main__" :
 	app=web.application(urls,globals())
 	app.run()
