70e050234f6e4edc29e60ef520613bfbc96c5763
shoaib11120 <mdshoaibkhan.1999@gmail.com>
2020-06-14 15:06:06 +0530

    Day 8! Added animation to choose image tag

--- static/js/designJ2.js
+++ static/js/designJ2.js
@@ -13,3 +13,17 @@ function blurFun(inn,sp) {
 	}
 	
 }
+function hoverFun(span) {
+			// body...
+			span.classList.add("FHover");
+		}
+		function nonhoverFun(span) {
+			// body...
+			span.classList.remove("FHover");
+		}
+		function DTextFun(inp){
+			alert(this.value)
+		}
+		function fun1(ev){
+			document.getElementById(ev).click();
+		}
